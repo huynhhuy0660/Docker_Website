ca26026edddd884c2ed74baaec23b255e32eeca7
huynhhuy0660 <huynhhuyy9c@gmail.com>
2026-04-13 14:03:50 +0700

    New

--- src/google_config.php
+++ src/google_config.php
@@ -1,10 +1,12 @@
 <?php
-// Đảm bảo không có khoảng trắng trước thẻ <?php
+// Client ID có thể để công khai
 $clientID = '764000185462-1alribgm2b9bsih0fcq5alp49lnps9q4.apps.googleusercontent.com';
-$clientSecret = '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';
 
-// HÃY KIỂM TRA DẤU GẠCH DƯỚI Ở ĐÂY
-$redirectUri = 'http://localhost:8080/web_ban_laptop/google_callback.php'; 
+// Lấy mã bí mật từ Biến môi trường (Environment Variable)
+// Trên máy tính cá nhân, bạn có thể dán mã vào phần sau dấu ?: để chạy thử
+$clientSecret = getenv('GOOGLE_CLIENT_SECRET') ?: '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';
+
+$redirectUri = 'https://laptop-store-vlu.onrender.com/google_callback.php'; 
 
 $login_url = "https://accounts.google.com/o/oauth2/v2/auth?scope=" . urlencode('email profile') . 
              "&redirect_uri=" . urlencode($redirectUri) . 
